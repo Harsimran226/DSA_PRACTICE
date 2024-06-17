1950a4024f82200812f28c79f27ed3b98ab110fb
Harman <146611938+Harsimran226@users.noreply.github.com>
2024-06-17 13:21:45 +0530

    Update PREFIX_TO_INFIX.c

--- PREFIX_TO_INFIX.c
+++ PREFIX_TO_INFIX.c
@@ -3,26 +3,26 @@
 #include<stdlib.h>
 #include<ctype.h>
 #define max 100
-void prefixtoinfix(char prefix[], char infix[]) {
+void prefixtoinfix(char source[], char target[]) {
     int i=0; 
     int k=0;
-    int len=strlen(prefix);
+    int len=strlen(source);
     char temp[100], s1[100][30];
-    
+    strcpy(target, "");
     for(i=len-1;i>=0;i--) {
-        if (isalnum(prefix[i])) {
-            char t[]={prefix[i],'\0'};
+        if (isalnum(source[i])) {
+            char t[]={source[i],'\0'};
             strcpy(s1[k++], t);
             
         } else {
             char *op1 = s1[--k]; 
             char *op2 = s1[--k]; 
-            sprintf(temp, "(%s%c%s)", op1, prefix[i], op2);
+            sprintf(temp, "(%s%c%s)", op1, source[i], op2);
             strcpy(s1[k++], temp); 
             
         }
     } 
-    strcpy(infix, s1[--k]); 
+    strcpy(target, s1[--k]); 
 }
 int main()
 {
